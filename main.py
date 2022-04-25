ac21aa6fd91ed3c00fbfb79b4bee7954257fa428
Khatia Khutsishvili <khkhutsishvili@tbcbank.com.ge>
2022-04-25 23:59:42 +0400

    added check if puzzle is solvable

--- main.py
+++ main.py
@@ -19,7 +19,16 @@ def printAsMatrix(list):
             print("")
         print(str(list[i]) + " ", end="")
 
-def move(indexes, position, state):
+def isSolvable(state):
+    invCount = 0
+    for i in range(0, 8):
+        for j in range(i + 1, 8):
+            if state[i] != 0 and state[j] != 0 and state[i] > state[j]:
+                invCount += 1
+
+    return invCount % 2 == 0
+
+def move(indexes, position, state, path):
     minMisplacedTilesNumber = sys.maxint
     storedState = list(state)
 
@@ -31,7 +40,7 @@ def move(indexes, position, state):
 
         misplacedTilesNumber = misplacedTiles(duplicatedState)
 
-        if misplacedTilesNumber < minMisplacedTilesNumber:
+        if misplacedTilesNumber < minMisplacedTilesNumber and duplicatedState not in path:
             minMisplacedTilesNumber = misplacedTilesNumber
             storedState = list(duplicatedState)
 
@@ -45,6 +54,7 @@ if __name__ == '__main__':
         x = input("Enter " + str(len(state)) + " element : ")
         state.append(x)
 
+    path = [state]
     heuristicValue = misplacedTiles(state)
     level = 1
 
@@ -52,32 +62,37 @@ if __name__ == '__main__':
     printAsMatrix(state)
     print("\nHeuristic Value : " + str(heuristicValue))
 
-    while heuristicValue > 0:
-        position = state.index(0)
-        level += 1
-        possibleIndexesOfEmptyTile = []
+    if isSolvable(state):
+        while heuristicValue > 0:
+            position = state.index(0)
+            level += 1
+            possibleIndexesOfEmptyTile = []
 
-        if position == 0:
-            possibleIndexesOfEmptyTile = [1, 3]
-        elif position == 1:
-            possibleIndexesOfEmptyTile = [0, 2, 4]
-        elif position == 2:
-            possibleIndexesOfEmptyTile = [1, 5]
-        elif position == 3:
-            possibleIndexesOfEmptyTile = [0, 4, 6]
-        elif position == 4:
-            possibleIndexesOfEmptyTile = [1, 3, 5, 7]
-        elif position == 5:
-            possibleIndexesOfEmptyTile = [2, 4, 8]
-        elif position == 6:
-            possibleIndexesOfEmptyTile = [3, 7]
-        elif position == 7:
-            possibleIndexesOfEmptyTile = [4, 6, 8]
-        elif position == 8:
-            possibleIndexesOfEmptyTile = [5, 6]
+            if position == 0:
+                possibleIndexesOfEmptyTile = [1, 3]
+            elif position == 1:
+                possibleIndexesOfEmptyTile = [0, 2, 4]
+            elif position == 2:
+                possibleIndexesOfEmptyTile = [1, 5]
+            elif position == 3:
+                possibleIndexesOfEmptyTile = [0, 4, 6]
+            elif position == 4:
+                possibleIndexesOfEmptyTile = [1, 3, 5, 7]
+            elif position == 5:
+                possibleIndexesOfEmptyTile = [2, 4, 8]
+            elif position == 6:
+                possibleIndexesOfEmptyTile = [3, 7]
+            elif position == 7:
+                possibleIndexesOfEmptyTile = [4, 6, 8]
+            elif position == 8:
+                possibleIndexesOfEmptyTile = [5, 7]
 
-        state, heuristicValue = move(possibleIndexesOfEmptyTile, position, state)
+            state, heuristicValue = move(possibleIndexesOfEmptyTile, position, state, path)
 
-        print("\n-- Level -- " + str(level))
-        printAsMatrix(state)
-        print("\nHeuristic Value : " + str(heuristicValue))
+            path.append(state)
+
+            print("\n-- Level -- " + str(level))
+            printAsMatrix(state)
+            print("\nHeuristic Value : " + str(heuristicValue))
+    else:
+        print("This puzzle is unsolvable")
